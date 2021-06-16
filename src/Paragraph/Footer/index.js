03be9199c904a330e698dcb7bfc67c742f5b2683
Renata Margula <reniutaaa@gmail.com>
2021-06-16 20:33:50 +0200

    changed class= for className=

--- src/Paragraph/Footer/index.js
+++ src/Paragraph/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './style.css';
 
 const Footer = () => (
-    <footer class="footer">
+    <footer className="footer">
         *Kurs z dnia 22.04.2021- według danych z Narodowego Banku Polskiego "Tabela nr 077/A/NBP/2021"
     </footer>
 );
