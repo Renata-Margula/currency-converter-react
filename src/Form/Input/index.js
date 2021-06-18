71413dd978b82ab896b7951ecd6c32e257eb584e
Renata Margula <reniutaaa@gmail.com>
2021-06-18 20:15:27 +0200

    unneeded react import

--- src/Form/Input/index.js
+++ src/Form/Input/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
 import './style.css';
 
-const Input = ({amount, setAmount}) => (
-    <input 
-    value={amount}
-    onChange={({target}) => setAmount(target.value)}
-    className="form__field" 
-    type="number" 
-    name="pln" 
-    step="any" 
-    min="0" 
-    required />
+const Input = ({ amount, setAmount }) => (
+    <input
+        value={amount}
+        onChange={({ target }) => setAmount(target.value)}
+        className="form__field"
+        type="number"
+        name="pln"
+        step="any"
+        min="0"
+        required />
 );
 
 export default Input;
