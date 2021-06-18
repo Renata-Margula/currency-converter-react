6d3016a1f9aaafd0137385fb7d01d80ee6d9e706
Renata Margula <reniutaaa@gmail.com>
2021-06-18 20:17:59 +0200

    toFixed moved to Form render

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -1,11 +1,8 @@
-import React from 'react';
 import './style.css';
 
 const Result = ({ result }) => (
-    
     <strong className="form__result">
-        {result.totalResult.toFixed(2)}&nbsp;{result.currency}
+        {result.totalResult}&nbsp;{result.currency}
     </strong>
 );
-
 export default Result;
