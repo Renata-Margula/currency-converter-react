551a636bdf6b3f0b9d41646d9a19735daa047f98
Renata Margula <reniutaaa@gmail.com>
2021-06-18 20:14:00 +0200

    changed App.js build

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React from 'react';
 import Container from './Container';
 import Form from './Form';
-import Paragraph from './Paragraph';
+import Footer from './Footer';
 
 function App() {
   return (
     <Container>
-      <Form>
-      </Form>
-      <Paragraph>
-      </Paragraph>
+      <Form />
+      <Footer
+        text='*Kurs z dnia 22.04.2021- według danych z Narodowego Banku Polskiego "Tabela nr 077/A/NBP/2021'
+      />
     </Container>
   );
-}
-
+};
 export default App;
