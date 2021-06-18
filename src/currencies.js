af083db048f1f44be5b17fd701f655a139a8f889
Renata Margula <reniutaaa@gmail.com>
2021-06-18 19:13:28 +0200

    moved currencies.js to src

---
+++ src/currencies.js
@@ -0,0 +1,8 @@
+const currencies = [
+    { id: "EUR", name: "EUR (euro)", rate: 4.5562 },
+    { id: "HUF", name: "HUF (forint (Węgry))", rate: 1.2544 },
+    { id: "GBP", name: "GBP (funt szterling)", rate: 5.2655 },
+    { id: "RON", name: "RON (lej rumuński)", rate: 0.925 },
+];
+
+export default currencies;
